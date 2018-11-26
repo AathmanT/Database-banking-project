c0a04cbde9db8cbd5a86b66c3e798130ec0852e4
Thineshan <thineshan.16@cse.mrt.ac.lk>
2018-11-26 18:01:23 +0530

    customer saving registration

--- CustomerAccount.php
+++ CustomerAccount.php
@@ -45,7 +45,7 @@
 </style>
 <body>
 
-<h3>Customer Registration </h3>
+<h3>Customer SavingAccount Registration </h3>
 
 <div>
     <form action="" method="post">
@@ -53,7 +53,15 @@
         <select type="text" id="accounttype" required name="accounttype">
             <option>AccountType..</option>
             <option>SavingAccount</option>
-            <option>CurrentAccount</option>
+        </select>
+
+        <label for="category">Category</label>
+        <select type="text" id="category" required name="category">
+            <option>Category..</option>
+            <option>Children</option>
+            <option>Teen</option>
+            <option>Adult(18+)</option>
+            <option>Senior(60+)</option>
         </select>
 
         <label for="customername">CustomerName</label>
@@ -75,14 +83,14 @@
         <input type="tel" pattern="[0-9]{10}" id="customerphoneno" required name="customerphoneno" placeholder="CustomerPhoneNo..">
 
         <label for="branchname">BranchName</label>
-            <select type="text" id="branchname" required name="branchname">
-                <option>BranchName..</option>
-                <option>Jaffna</option>
-                <option>Kaithadi</option>
-                <option>Nallur</option>
-                <option>Kobai</option>
-                <option>Kokuvil</option>
-            </select>
+        <select type="text" id="branchname" required name="branchname">
+            <option>BranchName..</option>
+            <option>Jaffna</option>
+            <option>Kaithadi</option>
+            <option>Nallur</option>
+            <option>Kobai</option>
+            <option>Kokuvil</option>
+        </select>
 
 
         <input type="submit" value="Submit">
@@ -97,6 +105,7 @@ $db=new mysqli('localhost','root','','banking');
 if(!empty($_POST)) {
 
     $AccountType = $_POST['accounttype'];
+    $Category=$_POST['category'];
     $CustomerName = $_POST['customername'];
     $CustomerAddress = $_POST['customeraddress'];
     $DateOfBirth = $_POST['dateofbirth'];
@@ -109,14 +118,16 @@ if(!empty($_POST)) {
  ('{$_POST['customername']}','{$_POST['customeraddress']}','{$_POST['dateofbirth']}','{$_POST['NIC']}','{$_POST['customeremail']}',
  '{$_POST['customerphoneno']}')");
 
-    $db->query("insert into account (AccountNo,BranchID,AccountType) values
- ('160002',select branchID from branch where branchName='{$_POST['branchname']}','{$_POST['accounttype']}')");
+    $db->query("insert into account (BranchID,AccountType) values
+ ((select branchID from branch where branchName='{$_POST['branchname']}'),'{$_POST['accounttype']}')");
+
+    //$db->query("insert into account()")
 
     //$db->query("insert into ")
 
-  //  var_dump($db->query("select last_insert_id() from account")->fetch_object());
+    //  var_dump($db->query("select last_insert_id() from account")->fetch_object());
 
 }
 //insert into account (AccountNo,BranchID,AccountType) values
-  //  ('160001',(select branchID from branch where branchName='Jaffna'),'SavingAccount')
+//  ('160001',(select branchID from branch where branchName='Jaffna'),'SavingAccount')
 ?>
